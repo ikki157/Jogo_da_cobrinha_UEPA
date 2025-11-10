f2f137532d52ba0cf782d049644cf3f798edf69b
oLua Dev <jose.gc.neto@aluno.uepa.br>
2025-11-10 05:24:05 -0300

    Feat: Implementa o núcleo da lógica (Engine) e o Tabuleiro

--- src/jogo/controles/Jogo.java
+++ src/jogo/controles/Jogo.java
@@ -1,4 +1,95 @@
 package jogo.controles;
 
+import jogo.modelo.Cobra;
+import jogo.modelo.Comida;
+import jogo.modelo.Posicao;
+import jogo.modelo.Tabuleiro;
+
 public class Jogo {
+
+    private final Tabuleiro tabuleiro;
+    private final Cobra cobra;
+    private final Comida comida;
+    private boolean gameOver;
+    private int score;
+
+    public Jogo(int largura, int altura) {
+        this.tabuleiro = new Tabuleiro(largura, altura);
+
+        Posicao posInicial = new Posicao(largura / 2, altura / 2);
+        this.cobra = new Cobra(posInicial, Cobra.Direcao.DIREITA);
+
+        this.comida = new Comida(largura, altura);
+        this.gerarNovaComida();
+
+        this.gameOver = false;
+        this.score = 0;
+    }
+
+    public void atualizar() {
+        if (gameOver) {
+            return;
+        }
+
+        cobra.mover();
+
+        if (verificarColisoes()) {
+            gameOver = true;
+            return;
+        }
+
+        if (verificarComida()) {
+            score += 10;
+            cobra.crescer();
+            gerarNovaComida();
+        }
+    }
+
+    private boolean verificarColisoes() {
+        Posicao cabeca = cobra.getCabeca();
+
+        if (tabuleiro.isForaDosLimites(cabeca)) {
+            return true;
+        }
+
+        if (cobra.colidiuComCorpo()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean verificarComida() {
+        return cobra.getCabeca().equals(comida.getPosicao());
+    }
+
+    private void gerarNovaComida() {
+        do {
+            comida.gerarNovaPosicao(tabuleiro.getLargura(), tabuleiro.getAltura());
+        } while (cobra.getCorpo().contains(comida.getPosicao()));
+    }
+
+    public void setDirecaoCobra(Cobra.Direcao novaDirecao) {
+        cobra.mudarDirecao(novaDirecao);
+    }
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    public Cobra getCobra() {
+        return cobra;
+    }
+
+    public Comida getComida() {
+        return comida;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
--- src/jogo/modelo/Tabuleiro.java
+++ src/jogo/modelo/Tabuleiro.java
@@ -1,4 +1,25 @@
 package jogo.modelo;
 
 public class Tabuleiro {
+    private final int largura;
+    private final int altura;
+
+    public Tabuleiro(int largura, int altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public boolean isForaDosLimites(Posicao pos) {
+        int x = pos.getPosicaoX();
+        int y = pos.getPosicaoY();
+        return x < 0 || x >= largura || y < 0 || y >= altura;
+    }
 }
